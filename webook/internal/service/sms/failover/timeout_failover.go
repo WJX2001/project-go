371dcd4fd624e421548acbe7ac0438086393832c
WJX2001 <1758807220@qq.com>
2025-02-12 18:28:49 +0800

    feat: 限流策略

---
+++ webook/internal/service/sms/failover/timeout_failover.go
@@ -0,0 +1,54 @@
+package failover
+
+import (
+	"context"
+	"project-go/webook/internal/service/sms"
+	"sync/atomic"
+)
+
+type TimeoutFailOverSMSService struct {
+	// 你的服务商
+	svcs []sms.Service
+	idx  int32
+	// 连续超时的个数
+	cnt int32
+	// 阈值
+	// 连续超时超过这个数字，就要切换
+	threshold int32
+}
+
+func NewTimeoutFailOverSMSService() sms.Service {
+	return &TimeoutFailOverSMSService{}
+}
+
+func (t *TimeoutFailOverSMSService) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
+	idx := atomic.LoadInt32(&t.idx)
+	cnt := atomic.LoadInt32(&t.cnt)
+	if cnt > t.threshold {
+		// 这里要切换 新的下标 往后挪一位
+		newIdx := (idx + 1) % int32(len(t.svcs))
+		if atomic.CompareAndSwapInt32(&t.idx, idx, newIdx) {
+			// 成功往后挪动了一位
+			atomic.StoreInt32(&t.cnt, cnt)
+		}
+		// else 就是出现并发 别人换成功了
+
+		// idx = newIdx
+		idx = atomic.LoadInt32(&t.idx)
+	}
+	svc := t.svcs[idx]
+	err := svc.Send(ctx, tpl, args, numbers...)
+	switch err {
+	case context.DeadlineExceeded:
+		atomic.AddInt32(&t.cnt, 1)
+	case nil:
+		// 你的连续状态被打断
+		atomic.StoreInt32(&t.cnt, 0)
+	default:
+		// 不知道什么错误
+		// 你可以考虑换下一个
+		// - 超时 可能是偶发的，我尽量再试试
+		// - 非超时 我直接下一个
+		return err
+	}
+}
--- webook/ioc/sms.go
+++ webook/ioc/sms.go
@@ -1,11 +1,15 @@
 package ioc
 
 import (
+	"github.com/redis/go-redis/v9"
 	"project-go/webook/internal/service/sms"
 	"project-go/webook/internal/service/sms/memory"
 )
 
-func InitSMSService() sms.Service {
-	// 换内存还是换别的
+func InitSMSService(cmd redis.Cmdable) sms.Service {
+	//// 换内存还是换别的
+	//svc := memory.NewService()
+	//
+	//return ratelimit.NewRateLimitSMSService
 	return memory.NewService()
 }
